0637bba672668fde4d8a38a49abd690a854dfd25
drb101005 <drb101005@gmail.com>
2025-10-29 19:09:32 +0530

    Update User.java

--- backend/src/main/java/com/studentforum/backend/model/User.java
+++ backend/src/main/java/com/studentforum/backend/model/User.java
@@ -1,10 +1,8 @@
 package com.studentforum.backend.model;
-
 import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
 import java.util.List;
-
 @Entity
 @Table(name = "users")
 @Data
